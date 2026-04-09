82094d647082bda5764f4e46bfd7a54c97739755
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-09 15:40:04 +0700

    feat: update import paths to use goodfoodapp instead of sukientotapp

--- mason/feature/__brick__/lib/features/{{role}}/{{name.snakeCase()}}/controller.dart
+++ mason/feature/__brick__/lib/features/{{role}}/{{name.snakeCase()}}/controller.dart
@@ -1,6 +1,6 @@
-import 'package:sukientotapp/core/utils/import/global.dart';
+import 'package:goodfoodapp/core/utils/import/global.dart';
 {{#use_repository}}
-import 'package:sukientotapp/domain/repositories/{{role}}/{{name.snakeCase()}}_repository.dart';
+import 'package:goodfoodapp/domain/repositories/{{role}}/{{name.snakeCase()}}_repository.dart';
 {{/use_repository}}
 class {{name.pascalCase()}}Controller extends GetxController {
   {{#use_repository}}

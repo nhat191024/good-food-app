d8741d8608bbcf4038f904240bf5b8de3c066f80
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-09 15:40:36 +0700

    feat: add global imports and core utility exports for the project

---
+++ lib/core/utils/import/global.dart
@@ -0,0 +1,27 @@
+// Global imports for the project
+export 'package:get/get.dart' hide MultipartFile, FormData;
+export 'package:getx_super_snackbar/getx_super_snackbar.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/foundation.dart';
+export 'package:forui/forui.dart';
+export 'package:intl/intl.dart' hide TextDirection;
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:pull_to_refresh/pull_to_refresh.dart' hide RefreshIndicator, RefreshIndicatorState;
+export 'package:flutter_animate/flutter_animate.dart';
+
+// Exporting core utilities
+export 'package:goodfoodapp/core/utils/app_snackbar.dart';
+export 'package:goodfoodapp/core/utils/logger.dart';
+export 'package:goodfoodapp/core/utils/format_utils.dart';
+export 'package:goodfoodapp/core/routes/pages.dart';
+export 'package:goodfoodapp/core/utils/app_images.dart';
+export 'package:goodfoodapp/core/utils/app_colors.dart';
+export 'package:goodfoodapp/core/services/localstorage_service.dart';
+export 'package:goodfoodapp/core/services/pusher_service.dart';
+export 'package:goodfoodapp/core/services/notification_service.dart';
+
+// Exporting context extensions
+export 'package:goodfoodapp/core/utils/context_extensions.dart';
+
+// Global exports for commonly used packages
+// Add other global exports if that package are frequently used across the app
